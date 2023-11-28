2f675c4c0969b19fcff4d0d1e854f467e7fc650c
belacan1998 <152223095+belacan1998@users.noreply.github.com>
2023-11-28 11:34:04 -0600

    Add files via upload

--- server.r
+++ server.r
@@ -1,26 +1,10 @@
-function(input, output) {
-  
-  output$main_plot <- renderPlot({
-    
-    hist(faithful$eruptions,
-         probability = TRUE,
-         breaks = as.numeric(input$n_breaks),
-         
-         #server code for changing bar colour
-         col = as.character(input$bar_col),
-         
-         xlab = "Duration (minutes)",
-         main = "Geyser eruption duration")
-    
-    if (input$individual_obs) {
-      rug(faithful$eruptions)
-    }
-    
-    if (input$density) {
-      dens <- density(faithful$eruptions,
-                      adjust = input$bw_adjust)
-      lines(dens, col = "forestgreen")
-    }
-    
+library(shiny)
+
+shinyServer(function(input,output) {
+  output$distPlot<-renderPlot({
+    View(faithful)
+    x<-faithful[,2]
+    bins<-seq(min(x),max(x),length.out=input$bins+1)
+    hist(x,breaks=bins,col='green',border='red')
   })
-}
+})
--- ui.r
+++ ui.r
@@ -1,37 +1,17 @@
-bootstrapPage(
-  
-  selectInput(inputId = "n_breaks",
-              label = "Number of bins in histogram (approximate):",
-              choices = c(10, 20, 35, 50),
-              selected = 20),
-  
-  checkboxInput(inputId = "individual_obs",
-                label = strong("Show individual observations"),
-                value = FALSE),
-  
-  checkboxInput(inputId = "density",
-                label = strong("Show density estimate"),
-                value = FALSE),
-  
-  
-  
-  
-  #ui code for changing bar colour
-  
-   selectInput(inputId = "bar_col",
-               label = "Select bar colour",
-               choices = c("lightblue", "purple", "green", "lavender"),
-               selected = "green"),
-  
-  
-  
-  plotOutput(outputId = "main_plot", height = "300px"),
-  
-  # Display this only if the density is shown
-  conditionalPanel(condition = "input.density == true",
-                   sliderInput(inputId = "bw_adjust",
-                               label = "Bandwidth adjustment:",
-                               min = 0.2, max = 2, value = 1, step = 0.2)
+library(shiny)
+
+shinyUI(fluidPage(
+  titlePanel("old faithful geyser data"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins",
+                  "# bins:",
+                  min=1,
+                  max=50,
+                  value=30)
+    ),
+    mainPanel(
+      plotOutput("distPlot")
+    )
   )
-  
-)
+))
